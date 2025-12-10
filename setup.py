ffd57500ca7d0d39a804ed900256bf1692bf405e
Camilo <camilorg@kth.se>
2025-12-10 23:42:57 +0100

    feat: Add setup.py to define the starterkits Python package and its dependencies.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='starterkits',
+    version='0.1.0',
+    description='Starter Data Kits for CCG tools',
+    author='Camilo Ramirez Gomez',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'pycountry',
+        'pygadm',
+        'osmnx',
+        'boto3',
+    ],
+    python_requires='>=3.6',
+)
